816ae0c0c2913904d754dec77ea55b8a46db098c
xiaobitipao <gougou23@163.com>
2017-03-16 23:59:22 +0800

    update

--- src/main/java/com/xiaobitipao/sample/test/thead/conn004/Singletion.java
+++ src/main/java/com/xiaobitipao/sample/test/thead/conn004/Singletion.java
@@ -5,7 +5,7 @@ public class Singletion {
     private Singletion() {
     }
 
-    private static Singletion single = new Singletion();
+    private static final Singletion single = new Singletion();
 
     public static Singletion getInstance() {
         return single;
