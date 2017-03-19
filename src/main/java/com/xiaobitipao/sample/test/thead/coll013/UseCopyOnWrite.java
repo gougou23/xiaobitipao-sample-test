1d23c7ad50432fdec6d6d2d5253656c55f6490b1
xiaobitipao <gougou23@163.com>
2017-03-19 22:12:55 +0800

    init

---
+++ src/main/java/com/xiaobitipao/sample/test/thead/coll013/UseCopyOnWrite.java
@@ -0,0 +1,13 @@
+package com.xiaobitipao.sample.test.thead.coll013;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class UseCopyOnWrite {
+
+    public static void main(String[] args) {
+
+        CopyOnWriteArrayList<String> cwal = new CopyOnWriteArrayList<String>();
+        CopyOnWriteArraySet<String> cwas = new CopyOnWriteArraySet<String>();
+    }
+}
