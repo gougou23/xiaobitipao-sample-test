4868ca8b1ec55693497b450ff444970849e973de
xiaobitipao <gougou23@163.com>
2017-03-19 22:51:06 +0800

    update

--- src/main/java/com/xiaobitipao/sample/test/thead/design016/Data.java
+++ src/main/java/com/xiaobitipao/sample/test/thead/design016/Data.java
@@ -1,34 +1,33 @@
-package com.bjsxt.height.design016;
+package com.xiaobitipao.sample.test.thead.design016;
 
 public final class Data {
 
-	private String id;
-	private String name;
-	
-	public Data(String id, String name){
-		this.id = id;
-		this.name = name;
-	}
-	
-	public String getId() {
-		return id;
-	}
+    private String id;
+    private String name;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public Data(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	@Override
-	public String toString(){
-		return "{id: " + id + ", name: " + name + "}";
-	}
-	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{id: " + id + ", name: " + name + "}";
+    }
 }
