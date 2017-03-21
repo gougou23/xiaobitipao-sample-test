db7f34a53d927012398286976e5f03fbb4859665
xiaobitipao <gougou23@163.com>
2017-03-21 18:45:52 +0800

    update

--- src/main/java/com/xiaobitipao/sample/test/thead/coll012/Tickets.java
+++ src/main/java/com/xiaobitipao/sample/test/thead/coll012/Tickets.java
@@ -1,45 +1,33 @@
 package com.xiaobitipao.sample.test.thead.coll012;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Vector;
 
 /**
- * 多线程使用Vector或者HashTable的示例（简单线程同步问题）
- * 
- * @author alienware
+ * <pre>
+ * 多线程使用 Vector 或者 HashTable 的简单线程同步问题示例
+ * </pre>
  */
 public class Tickets {
 
     public static void main(String[] args) {
-        // 初始化火车票池并添加火车票:避免线程同步可采用Vector替代ArrayList HashTable替代HashMap
 
+        // 初始化火车票池并添加火车票:避免线程同步可采用 Vector 替代 ArrayList，HashTable 替代 HashMap
         final Vector<String> tickets = new Vector<String>();
+        for (int i = 1; i <= 1000; i++) {
+            tickets.add("火车票-" + i);
+        }
 
         // Map<String, String> map = Collections.synchronizedMap(new
         // HashMap<String, String>());
 
-        for (int i = 1; i <= 1000; i++) {
-            tickets.add("火车票" + i);
-        }
-
-        // for (Iterator iterator = tickets.iterator(); iterator.hasNext();) {
-        // String string = (String) iterator.next();
-        // tickets.remove(20);
-        // }
-
         for (int i = 1; i <= 10; i++) {
-            new Thread("线程" + i) {
-                public void run() {
-                    while (true) {
-                        if (tickets.isEmpty())
-                            break;
-                        System.out.println(Thread.currentThread().getName() + "---" + tickets.remove(0));
-                    }
+            new Thread(() -> {
+                while (true) {
+                    if (tickets.isEmpty())
+                        break;
+                    System.out.println(Thread.currentThread().getName() + "---" + tickets.remove(0));
                 }
-            }.start();
+            }, "线程-" + i).start();
         }
     }
 }
