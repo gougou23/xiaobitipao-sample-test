d47b1dea4c82e731509b6abb92a58915da6c50a1
xiaobitipao <gougou23@163.com>
2017-04-05 11:05:15 +0800

    update

--- src/main/java/com/xiaobitipao/sample/test/netty/runtime/Request.java
+++ src/main/java/com/xiaobitipao/sample/test/netty/runtime/Request.java
@@ -1,33 +1,36 @@
-package bhz.netty.runtime;
+package com.xiaobitipao.sample.test.netty.runtime;
 
 import java.io.Serializable;
 
-public class Request implements Serializable{
+public class Request implements Serializable {
 
-	private static final long  SerialVersionUID = 1L;
-	
-	private String id ;
-	private String name ;
-	private String requestMessage ;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getRequestMessage() {
-		return requestMessage;
-	}
-	public void setRequestMessage(String requestMessage) {
-		this.requestMessage = requestMessage;
-	}
+    private static final long serialVersionUID = 5664290875516399010L;
 
+    private String id;
+    private String name;
+    private String requestMessage;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRequestMessage() {
+        return requestMessage;
+    }
+
+    public void setRequestMessage(String requestMessage) {
+        this.requestMessage = requestMessage;
+    }
 }
--- src/main/java/com/xiaobitipao/sample/test/netty/runtime/Server.java
+++ src/main/java/com/xiaobitipao/sample/test/netty/runtime/Server.java
@@ -1,4 +1,4 @@
-package bhz.netty.runtime;
+package com.xiaobitipao.sample.test.netty.runtime;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
@@ -14,31 +14,48 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 
 public class Server {
 
-	public static void main(String[] args) throws Exception{
-		
-		EventLoopGroup pGroup = new NioEventLoopGroup();
-		EventLoopGroup cGroup = new NioEventLoopGroup();
-		
-		ServerBootstrap b = new ServerBootstrap();
-		b.group(pGroup, cGroup)
-		 .channel(NioServerSocketChannel.class)
-		 .option(ChannelOption.SO_BACKLOG, 1024)
-		 //设置日志
-		 .handler(new LoggingHandler(LogLevel.INFO))
-		 .childHandler(new ChannelInitializer<SocketChannel>() {
-			protected void initChannel(SocketChannel sc) throws Exception {
-				sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingDecoder());
-				sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingEncoder());
-				sc.pipeline().addLast(new ReadTimeoutHandler(5)); 
-				sc.pipeline().addLast(new ServerHandler());
-			}
-		});
-		
-		ChannelFuture cf = b.bind(8765).sync();
-		
-		cf.channel().closeFuture().sync();
-		pGroup.shutdownGracefully();
-		cGroup.shutdownGracefully();
-		
-	}
+    // 服务端监听端口
+    private final static int PORT = 8765;
+
+    public void bind(int port) throws Exception {
+
+        // 1.第一个线程组:是用于接收 Client 端连接的
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+
+        // 2.第二个线程组:是用于处理已经接收到的连接
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(bossGroup, workerGroup);
+            sb.channel(NioServerSocketChannel.class);
+            sb.option(ChannelOption.SO_BACKLOG, 1024);
+            // 设置日志
+            sb.handler(new LoggingHandler(LogLevel.INFO));
+            sb.childHandler(new ChannelInitializer<SocketChannel>() {
+                protected void initChannel(SocketChannel sc) throws Exception {
+                    // 设置通过 Jboss Marshalling 进行序列化和反序列化
+                    sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingDecoder());
+                    sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingEncoder());
+
+                    // 超时handler（当服务器端与客户端在指定时间以上没有进行任何通信，则会关闭响应的通道，主要为减小服务端资源占用）
+                    sc.pipeline().addLast(new ReadTimeoutHandler(5));
+
+                    sc.pipeline().addLast(new ServerHandler());
+                }
+            });
+
+            ChannelFuture cf = sb.bind(port).sync();
+
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Server().bind(PORT);
+    }
 }
